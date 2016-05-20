ad28c88a901108913b94ad4f7f231e3ee40928d3
shiliang913 <570352648@qq.com>
2016-05-20 15:14:26 +0800

    520t

---
+++ src/com/Demo.java
@@ -0,0 +1,62 @@
+package com;
+
+import io.appium.java_client.AppiumDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities; 
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;  
+import java.util.List;
+
+ 
+public class Demo {
+	
+    private AppiumDriver driver; 
+    
+    @Before
+    public void setUp() throws Exception {
+        //设置apk的路径
+        File classpathRoot = new File(System.getProperty("user.dir"));
+        File appDir = new File(classpathRoot, "apps");
+        File app = new File(appDir, "ContactManager.apk");
+        
+        //设置自动化相关参数
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability(CapabilityType.BROWSER_NAME, "");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "Android Emulator");
+        //设置安卓系统版本
+        capabilities.setCapability("platformVersion", "6.0");
+        //设置apk路径
+        capabilities.setCapability("app", app.getAbsolutePath()); 
+        //设置app的主包名和主类名
+        capabilities.setCapability("appPackage", "com.example.android.contactmanager");
+        capabilities.setCapability("appActivity", ".ContactManager");
+        
+        //初始化
+        driver = new AppiumDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);       
+    }
+ 
+    @Test
+    public void addContact(){
+        WebElement el = driver.findElement(By.name("Add Contact"));
+        el.click();
+        List<WebElement> textFieldsList = driver.findElementsByClassName("android.widget.EditText");
+        textFieldsList.get(0).sendKeys("Tony");
+        textFieldsList.get(1).sendKeys("13800138000");
+        textFieldsList.get(2).sendKeys("demo@example.com");
+        driver.swipe(100, 500, 100, 100, 2);
+        driver.findElementByName("Save").click();
+    }    
+    
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+}
